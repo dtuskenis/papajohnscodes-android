95c49fa4d094c77dded08781f22b274b3493ca64
Denis Tuskenis <dtuskenis@qulix.com>
2019-03-27 13:33:52 +0300

    implement api

--- app/src/main/java/com/dtuskenis/papajohnscodes/MainActivity.kt
+++ app/src/main/java/com/dtuskenis/papajohnscodes/MainActivity.kt
@@ -2,11 +2,34 @@ package com.dtuskenis.papajohnscodes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
 
 class MainActivity: AppCompatActivity() {
+
+    private var apiRequest: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        apiRequest = PapaJohnsCodesProvider.codes
+            .subscribeBy(
+                onSuccess = {
+                    toast("Success: $it")
+                },
+                onError = {
+                    toast("Error: $it")
+                }
+            )
     }
+
+    override fun onDestroy() {
+        apiRequest?.dispose()
+        apiRequest = null
+
+        super.onDestroy()
+    }
+
+    private fun toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
---
+++ app/src/main/java/com/dtuskenis/papajohnscodes/PapaJohnsCode.kt
@@ -0,0 +1,4 @@
+package com.dtuskenis.papajohnscodes
+
+data class PapaJohnsCode(val name: String,
+                         val code: String)
