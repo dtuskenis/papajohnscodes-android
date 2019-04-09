2f0e2a0c0940f771d58748dc96dfe40b86a40354
Denis Tuskenis <dtuskenis@qulix.com>
2019-04-09 12:52:03 +0300

    utilize AutoCloseable::use

--- app/src/androidTest/java/com/dtuskenis/papajohnscodes/test/TestPromoCodesProvider.kt
+++ app/src/androidTest/java/com/dtuskenis/papajohnscodes/test/TestPromoCodesProvider.kt
@@ -6,25 +6,18 @@ import com.dtuskenis.papajohnscodes.PromoCodesProvider
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import io.reactivex.Single
-import java.io.InputStream
 
 class TestPromoCodesProvider(private val assets: AssetManager): PromoCodesProvider {
 
     private val promoCodesTypeToken = object : TypeToken<List<PromoCode>>(){}
 
     override val codes: Single<List<PromoCode>> =
-        Single.fromCallable { readText { assets.open("promo_codes.json") } }
+        Single.fromCallable { loadPromoCodesJson() }
               .map { parsePromoCodes(it) }
 
-    private fun readText(openStream: () -> InputStream): String {
-        var inputStream: InputStream? = null
-        try {
-            inputStream = openStream()
-            return inputStream.bufferedReader().readText()
-        } finally {
-            inputStream?.close()
-        }
-    }
+    private fun loadPromoCodesJson(): String =
+        assets.open("promo_codes.json").use { it.bufferedReader().readText() }
 
-    private fun parsePromoCodes(json: String): List<PromoCode> = Gson().fromJson(json, promoCodesTypeToken.type)
+    private fun parsePromoCodes(json: String): List<PromoCode> =
+        Gson().fromJson(json, promoCodesTypeToken.type)
 }
