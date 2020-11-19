b0754cdc68ee9e5038a2b146e2bfece7afd89b5b
Denis Tuskenis <denistuskenis@gmail.com>
2020-11-19 17:58:19 +0300

    update retrofit and replace rxJava with coroutines

--- app/src/main/java/com/dtuskenis/papajohnscodes/PromoCodesProvider.kt
+++ app/src/main/java/com/dtuskenis/papajohnscodes/PromoCodesProvider.kt
@@ -1,8 +1,6 @@
 package com.dtuskenis.papajohnscodes
 
-import io.reactivex.Single
-
 interface PromoCodesProvider {
 
-    val codes: Single<List<PromoCode>>
+    suspend fun getCodes(): List<PromoCode>
 }
